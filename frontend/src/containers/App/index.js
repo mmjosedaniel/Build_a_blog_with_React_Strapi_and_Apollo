a708c8919ef097e74390e406f940d636557a0a0f
Daniel Martínez <mmjosedaniel@hotmail.com>
2020-06-30 09:31:15 -0500

    blog completed

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-import Articles from "../Articles";
+import { Switch, Route } from "react-router-dom";
+
 import Nav from "../../components/Nav";
+import Articles from "../Articles";
+import Article from "../Article";
+import Category from "../Category";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Articles />
+      <Switch>
+        <Route path="/" component={Articles} exact />
+        <Route path="/article/:id" component={Article} exact />
+        <Route path="/category/:id" component={Category} exact />
+      </Switch>
     </div>
   );
 }
